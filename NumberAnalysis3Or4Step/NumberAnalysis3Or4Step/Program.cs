be67b946b4fac430665bc46fa27be6cb652d45f6
Yunus Emre Dal <35889523+yunusemredal@users.noreply.github.com>
2022-02-13 22:57:42 +0300

    Add files via upload

---
+++ NumberAnalysis3Or4Step/NumberAnalysis3Or4Step/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAnalysis3Or4Step
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            /* Türkçe: 3 basamaklı ise rakamları toplar.4 basamaklı ise 4 ile bölümününden kalanı ,
+             5 basamaklı ise 5 ile böümünden kalanı kontrol eder. 
+            English: This program Analysis number 3-4-5 digit . Adding the digits of a 3-digit number,
+            calculating the remainder from dividing a 4-digit number by 4 , 
+            calculating the remainder from dividing a 5-digit number by 5. 
+            */ 
+            Console.WriteLine("Please enter the number. Your number mustbe 3-4-5 digit. ");
+            int number, DigitNumber;
+            number = Convert.ToInt32(Console.ReadLine());
+            DigitNumber = Convert.ToString(number).Length;
+            Console.WriteLine("Number of digit:" + DigitNumber + " digit.");
+            if (DigitNumber == 3)
+            {
+                int hundreds, tens, ones, squares;
+                hundreds = number / 100;
+                tens = (number - (hundreds * 100)) / 10;
+                ones = ((number - (hundreds * 100)) - (tens * 10));
+                squares = (hundreds * hundreds) + (tens * tens) + (ones * ones);
+                Console.WriteLine(number + " " +squares);
+                
+            }
+            else if (DigitNumber == 4)
+            {
+                int mod = number % 4;
+                if (mod == 0)
+                {
+                    Console.WriteLine("This number dividing by 4  without a remainder.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("This number isn't dividing by 4 without a remainder. ");
+                    int remainderFour = number % 4; 
+                    Console.WriteLine("Remainder:"+remainderFour);
+                }
+            }
+            else if (DigitNumber==5)
+            {
+                int mod5 = number % 5;
+                switch (mod5)
+                {
+                    case 0:
+                        Console.WriteLine("This number dividing by 5 without remainder.");
+                        break;
+                    default:
+                        Console.WriteLine("This number isn't dividing by 5 without remainder.");
+                        int remainderFive = number % 5;
+                        Console.WriteLine("Remainder:"+remainderFive);
+                        break;
+                } 
+            }
+            else
+            {
+                Console.WriteLine("You entered wrong number. Please try again later");
+            }
+            Console.WriteLine("Thanks for choosing us. ");
+            /*
+                Basit seviyede ingilizce kullanıldığından türkçe olarak güncellenebilir bir kod parçasıdır. 
+             */
+
+        }
+    }
+}
---
+++ PerfectNumberCalculators/PerfectNumberCalculators/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace PerfectNumberCalculators
+{
+  internal class Program
+    {
+        public static void Main(string[] args)
+        { 
+            /*
+             Türkçe: Bu program mükemmel sayı bulmak için yazılmıştır.
+             Mükemmel sayı, sayılar teorisinde, kendisi hariç pozitif tam bölenlerinin toplamı kendisine eşit olan sayı. 
+             English: This program is written to find perfect number. 
+             Perfeckt Number , In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+             */
+            Console.WriteLine("Please enter the number.");
+            int number = Convert.ToInt32((Console.ReadLine()));
+            int total = 0;
+            string divid = "";
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    total = total + i;
+                    divid += i.ToString() + ' ';
+                    
+
+                }
+            }
+            Console.WriteLine(divid);
+            if (total==number)
+            {
+                Console.WriteLine(number + "this number is Perfect Number");
+                Console.WriteLine(divid + " = " + number);
+                
+            }
+            else
+            {
+                Console.WriteLine(number+ "This isn't Perfect Number");
+                
+            }
+            
+            
+        }
+        
+    }
+}
